3d87673933521c91363837e2a07f551111848e37
Manish <mkjha9555@gmail.com>
2017-12-21 23:37:44 +0530

    remove user

--- src/routes/list.js
+++ src/routes/list.js
@@ -2,11 +2,11 @@ import { route } from "./";
 import BaseClass from "../db/list";
 export const usaCompanyList = route(async (req, res) => {
   let db = req.db;
-  const users = await BaseClass.listOfAllCompanyDetailsInUSA(db);
-  res.send({ data: users });
+  const list = await BaseClass.listOfAllCompanyDetailsInUSA(db);
+  res.send({ data: list });
 });
 export const indianStateList = route(async (req, res) => {
   let db = req.db;
-  const users = await BaseClass.listOfAllIndianState(db);
-  res.send({ data: users });
+  const list = await BaseClass.listOfAllIndianState(db);
+  res.send({ data: list });
 });
