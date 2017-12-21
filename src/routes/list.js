54ebb98b3428059dfb458cda8aac2afd3d3cc4d2
Manish <mkjha9555@gmail.com>
2017-12-21 23:40:28 +0530

    change class name

--- src/routes/list.js
+++ src/routes/list.js
@@ -1,12 +1,12 @@
 import { route } from "./";
-import BaseClass from "../db/list";
+import BaseDB from "../db/list";
 export const usaCompanyList = route(async (req, res) => {
   let db = req.db;
-  const list = await BaseClass.listOfAllCompanyDetailsInUSA(db);
+  const list = await BaseDB.listOfAllCompanyDetailsInUSA(db);
   res.send({ data: list });
 });
 export const indianStateList = route(async (req, res) => {
   let db = req.db;
-  const list = await BaseClass.listOfAllIndianState(db);
+  const list = await BaseDB.listOfAllIndianState(db);
   res.send({ data: list });
 });
